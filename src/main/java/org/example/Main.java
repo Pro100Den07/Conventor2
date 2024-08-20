f16968d99ca143b4eaa18a4cdcee2ba012aa6b10
Pro100Den07 <drozhkov07@gmail.com>
2024-08-20 19:32:59 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Проект: Converter");
+        System.out.println("Версія: 1.0");
+        System.out.println("Автор: Ваше ім'я");
+    }
+}
